4cdf7f074a8b12e5855b68aa0c4bb93fdb3d70e0
parshantdhall <34908078+parshantdhall@users.noreply.github.com>
2018-12-22 14:07:00 +0530

    Added python version control on installer

--- install.py
+++ install.py
@@ -1,12 +1,18 @@
 import os
+import sys
 import platform
 
-if (platform.system() != "Windows"):
-    os.system('sudo apt-get install python-pip')
-    os.system('sudo pip install python-whois clearbit shodan fullcontact.py requests')
+def exec_api():
+    os.system('python main/gen_api.py')
+    os.system('python reconspider.py')
+
+if sys.version_info[0] < 3:
+    if (platform.system() != "Windows"):
+        os.system('sudo apt-get install python-pip')
+        os.system('sudo pip install python-whois clearbit shodan fullcontact.py requests')
+        exec_api()
+    else:
+        os.system('C:\Python27\Scripts\pip install python-whois clearbit shodan fullcontact.py requests')
+        exec_api()
 else:
-    os.system('C:\Python27\Scripts\pip install python-whois clearbit shodan fullcontact.py requests')
-
-
-os.system('python main/gen_api.py')
-os.system('python reconspider.py')
+    print("Your System Python Version "+ str(sys.version_info[0]) + " isn't compatible with ReconSpider. Use Python 2.7")
