1ff231bb63b03a734d8012fa9832b92b106a9c86
Bhav Karan <bhav.chahal97@gmail.com>
2019-01-03 22:23:49 +0530

    Update log

--- core/update_log.py
+++ core/update_log.py
@@ -1,1 +1,1 @@
-changes = '''Update usage info of reconspider.py;Added link to ReconSpider Wiki Guide;Added Development board and Contact details in README.md;Rename args file to repl_prompt;DNS Map image not auto-open Fix'''
+changes = '''Update feature support for Python 2;Update feature Bug Fixes;New Update feature for ReconSpider;Update usage info of reconspider.py;Added link to ReconSpider Wiki Guide;Added Development board and Contact details in README.md;Rename args file to repl_prompt;DNS Map image not auto-open Fix'''
