f4718f6ed02685fd5e3f5c74c46a1d85928e87c3
Bhav Karan <bhav.chahal97@gmail.com>
2019-01-03 17:13:07 +0530

    Update feature Bug Fixes

--- core/updater.py
+++ core/updater.py
@@ -1,33 +1,42 @@
 import os
 import re
+import sys
 from requests import get
 from .update_log import changes
 
-from .colors import run, que, good, info, end, green
+from .colors import run, que, good, bad, info, end, green
 
 
 def update():
-    print('%sChecking for updates..' % run)
-    latestCommit = get('https://raw.githubusercontent.com/bhavsec/reconspider/master/core/update_log.py').text
+    if sys.version_info[0] > 2:
+        print('\n%s Checking for updates..' % run)
+        latestCommit = get('https://raw.githubusercontent.com/bhavsec/reconspider/master/core/update_log.py').text
 
-    if changes not in latestCommit:  # just a hack to see if a new version is available
-        changelog = re.search(r"changes = '''(.*?)'''", latestCommit)
-        changelog = changelog.group(1).split(';')  # splitting the changes to form a list
-        print('%s\nA new version of ReconSpider is available.' % good)
-        print('%s\nChanges:' % info)
-        for change in changelog:  # print changes
-            print('%s>%s %s' % (green, end, change))
+        if changes not in latestCommit:  # just a hack to see if a new version is available
+            changelog = re.search(r"changes = '''(.*?)'''", latestCommit)
+            changelog = changelog.group(1).split(';')  # splitting the changes to form a list
+            print('\n%s A new version of ReconSpider is available.' % good)
+            print('\n%s Changes:' % info)
+            for change in changelog:  # print changes
+                print('%s>%s %s' % (green, end, change))
 
-        currentPath = os.getcwd().split('/')  # if you know it, you know it
-        folder = currentPath[-1]  # current directory name
-        path = '/'.join(currentPath)  # current directory path
-        choice = input('%s\nWould you like to update? [Y/n] ' % que).lower()
+            currentPath = os.getcwd().split('/')  # if you know it, you know it
+            folder = currentPath[-1]  # current directory name
+            path = '/'.join(currentPath)  # current directory path
+            choice = input('\n%s Would you like to update? [Y/n] ' % que).lower()
 
-        if choice != 'n':
-            print('%s\nUpdating ReconSpider..' % run)
-            os.system(
-                'git clone --quiet https://github.com/bhavsec/reconspider %s' % (folder))
-            os.system('cp -r %s/%s/* %s && rm -r %s/%s/ 2>/dev/null' % (path, folder, path, path, folder))
-            print('%s\nUpdate successful!' % good)
+            if choice == 'y':
+                print('\n%s Updating ReconSpider..' % run)
+                os.system('git clone --quiet https://github.com/bhavsec/reconspider %s' % (folder))
+                os.system('cp -r %s/%s/* %s && rm -r %s/%s/ 2>/dev/null' % (path, folder, path, path, folder))
+                print('\n%s Update successful!' % good)
+                sys.exit()
+            
+            else:
+                print('\n%s Update Canceled!' % bad)
+
+        else:
+            print('\n%s ReconSpider is up to date!' % good)
+
     else:
-        print('%s ReconSpider is up to date!' % good)
+        print('\n%s Sorry! Update feature is currently available for python 3 only' % bad)
