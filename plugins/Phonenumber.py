e9f76844d75eee6f27630df1d90b8ea622d7f671
T3cH_W1z4rD <52521300+Aravindha1234u@users.noreply.github.com>
2020-06-26 19:57:08 +0530

    Issue #29 and #27 has been fixed
    Since free API has only 250 request per month, Once your limit is reached, you wont be able to access it or use that module.

--- plugins/Phonenumber.py
+++ plugins/Phonenumber.py
@@ -9,17 +9,20 @@ def Phonenumber(ph):
         exit()
     url = ("http://apilayer.net/api/validate?access_key="+api_key+"&number="+str(ph))
     response=requests.get(url)
-    if "91" not in str(ph):
-        print("Error: CountryCode is missing")
-    else:
-        if response.status_code ==200:
-            get=response.json()
-            print("Number: "+get['number'])
-            print("Type: "+get['line_type'])
-            print("CountryCode: "+get['country_code'])
-            print("Country: "+get['country_name'])
-            print("Location: "+get['location'])
-            print("Carrier: "+get['carrier'])
-            print("")
+    try:
+        if "91" not in str(ph):
+            print("Error: CountryCode is missing")
         else:
-            print("Error: Invalid Mobile Number")
+            if response.status_code ==200:
+                get=response.json()
+                print("Number: "+get['number'])
+                print("Type: "+get['line_type'])
+                print("CountryCode: "+get['country_code'])
+                print("Country: "+get['country_name'])
+                print("Location: "+get['location'])
+                print("Carrier: "+get['carrier'])
+                print("")
+            else:
+                print("Error: Invalid Mobile Number")
+    except:
+        print(str(response.json()['error']['info']).split(".")[0])
